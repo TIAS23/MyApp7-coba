903b4f0ad4952c81da6efd4cdcd40201e3e6633d
TIAS23 <96928599+TIAS23@users.noreply.github.com>
2023-09-25 08:57:35 +0700

    Initial commit

---
+++ app/src/main/java/com/tiashl/myapp7/onboarding/screens/FirstScreen.kt
@@ -0,0 +1,28 @@
+package com.tiashl.myapp7.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.tiashl.myapp7.R
+
+class FirstScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first_screen, container, false)
+
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+
+        view.findViewById<View>(R.id.next).setOnClickListener {
+            viewPager?.currentItem = 1
+        }
+
+        return view
+    }
+}
